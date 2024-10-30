4525046778628c42e222259dc587fc5c4e4356eb
TylerJoseph_Kellogg <yaz8bp@virginia.edu>
2024-10-29 22:00:05 -0400

    Initial commit of Module9demo package

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='mypackage',
+    version='0.1',
+    description='A simple demonstration package',
+    url='http://github.com/tylerkellogg/module9demo',
+    author='Tyler Kellogg',
+    author_email='ontoligent@gmail.com',
+    license='MIT',
+    packages=['mypackage']
+)
